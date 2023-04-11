b110d39b5b6776ae2f78c27d45ee362c45f20ab0
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-04-11 11:10:31 +0200

    CU-862jju5nw__Add-ecsb-moving-connectivity * Create MessageHandler with message types * Create function for parsing messages * Create function for sending messages over websocket * Handle incoming messages properly

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,31 +1,39 @@
 import Phaser from 'phaser'
-import GridEngine from "grid-engine";
-import { Scene } from './scenes/Scene';
+import GridEngine from 'grid-engine'
+import { Scene } from './scenes/Scene'
 
-export let game: Phaser.Game|null = null;
+let game: Phaser.Game | null = null
 
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  render: {
-    antialias: false,
-  },
-  scale: {
-    mode: Phaser.Scale.ScaleModes.RESIZE,
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
-  parent: "game-content",
-  scene: Scene,
-  plugins: {
-    scene: [
-      {
-        key: "gridEngine",
-        plugin: GridEngine,
-        mapping: "gridEngine", 
-      },
-    ],
-  },
-};
+export const createGame = (): void => {
+  const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    render: {
+      antialias: false,
+    },
+    scale: {
+      mode: Phaser.Scale.ScaleModes.RESIZE,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+    parent: 'game-content',
+    scene: Scene,
+    plugins: {
+      scene: [
+        {
+          key: 'gridEngine',
+          plugin: GridEngine,
+          mapping: 'gridEngine',
+        },
+      ],
+    },
+  }
 
-export const createGame = () => { game = new Phaser.Game(config)};
-export const endGame = () => { game?.destroy(true) };
+  game = new Phaser.Game(config)
+}
+
+export const endGame = (): void => {
+  if (game) {
+    game.destroy(true)
+    game = null
+  }
+}
--- src/Game/scenes/Scene.ts
+++ src/Game/scenes/Scene.ts
@@ -1,159 +1,169 @@
-import { Direction } from "grid-engine";
-import type { GridEngine } from "grid-engine";
-import * as Phaser from "phaser";
-import { WebsocketBuilder} from 'websocket-ts';
-import type { Websocket } from 'websocket-ts';
+import * as Phaser from 'phaser'
+import { Direction } from 'grid-engine'
+import type { GridEngine } from 'grid-engine'
+import { WebsocketBuilder } from 'websocket-ts'
+import type { Websocket } from 'websocket-ts'
+import { MessageType, parseMessage, sendMessage } from '../MessageHandler'
+import type { Coordinates } from '../MessageHandler'
+import { v4 as uuidv4 } from 'uuid'
 
-interface position {
-  x: number; 
-  y: number;
-}
-
-function makeid(length: number) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-};
-
-const API_URL: string = import.meta.env.VITE_API_URL as string;
+const sessionID = 10
+const playerID = uuidv4()
+const players: Record<string, Coordinates> = {}
+const ECSB_MOVE_API_URL: string = import.meta.env.VITE_ECSB_MOVE_API_URL as string
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
-  key: "Game",
-};
-const playerID = makeid(10);
-const players: { [id: string]: position } = {};
+  key: 'Game',
+}
 
 export class Scene extends Phaser.Scene {
-  private readonly gridEngine!: GridEngine;
-  private readonly ws!: Websocket;
+  private readonly gridEngine!: GridEngine
+  private ws!: Websocket
 
   constructor() {
-    super(sceneConfig);
-
-    this.ws = new WebsocketBuilder(`${API_URL}/ws?sessionId=asdf&name=${playerID}`)
-        .onOpen((i, ev) => { console.log("ws openen") })
-        .onClose((i, ev) => { console.log("ws closed") })
-        .onError((i, ev) => { console.log("ws error") })
-        .onMessage((i, ev) => {
-          const msg: {message?: {type: string, id: string, x: number, y: number}} = JSON.parse(ev.data);
-
-          if (!msg.message) {
-            return;
-          }
-
-          switch (msg.message.type) {
-            case "player_added":
-              this.addPlayer(msg.message.id, {x: msg.message.x, y: msg.message.y});
-              break;
-            case "player_moved":
-              this.movePlayer(msg.message.id, {x: msg.message.x, y: msg.message.y});
-              break;
-            case "player_remove":
-              this.removePlayer(msg.message.id);
-              break;
-          }
-        })
-        .onRetry((i, ev) => { console.log("retry") })
-        .build();
+    super(sceneConfig)
   }
 
-  preload() {
-    this.load.image("tiles", "src/assets/cloud_tileset.png");
-    this.load.tilemapTiledJSON("cloud-city-map", "src/assets/cloud_city.json");
-    this.load.spritesheet("player", "src/assets/characters.png", {
+  preload(): void {
+    this.load.image('tiles', 'src/assets/cloud_tileset.png')
+    this.load.tilemapTiledJSON('cloud-city-map', 'src/assets/cloud_city.json')
+    this.load.spritesheet('player', 'src/assets/characters.png', {
       frameWidth: 52,
       frameHeight: 72,
-    });
+    })
   }
 
-  create() {
-    const cloudCityTilemap = this.make.tilemap({ key: "cloud-city-map" });
-    cloudCityTilemap.addTilesetImage("Cloud City", "tiles");
+  create(): void {
+    const cloudCityTilemap = this.make.tilemap({ key: 'cloud-city-map' })
+    cloudCityTilemap.addTilesetImage('Cloud City', 'tiles')
+
     for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
-      const layer = cloudCityTilemap.createLayer(i, "Cloud City", 0, 0);
-      layer.scale = 3;
+      const layer = cloudCityTilemap.createLayer(i, 'Cloud City', 0, 0)
+      layer.scale = 3
     }
 
-    const playerSprite = this.add.sprite(0, 0, "player");
-
-    this.cameras.main.startFollow(playerSprite, true);
-    this.cameras.main.setFollowOffset(
-      -playerSprite.width,
-      -playerSprite.height
-    );
+    const playerSprite = this.add.sprite(0, 0, 'player')
+    this.cameras.main.startFollow(playerSprite, true)
+    this.cameras.main.setFollowOffset(-playerSprite.width, -playerSprite.height)
 
     const gridEngineConfig = {
       characters: [
         {
-          id: "player",
+          id: playerID,
           sprite: playerSprite,
           walkingAnimationMapping: 6,
           startPosition: { x: 3, y: 3 },
-          collides: false
-        }
+          collides: false,
+        },
       ],
-    };
+    }
 
-    this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
+    this.gridEngine.create(cloudCityTilemap, gridEngineConfig)
 
-    this.gridEngine.positionChangeStarted().subscribe(({charId, exitTile, enterTile}) => {
-      this.ws.send(JSON.stringify({
-        "type": "player_moved",
-        "id": playerID,
-        "x": enterTile.x,
-        "y": enterTile.y
-      }))
-    });
+    this.configureWebSocket()
+
+    this.gridEngine.positionChangeStarted().subscribe(({ charId, exitTile, enterTile }) => {
+      if (charId === playerID) {
+        sendMessage(this.ws, {
+          type: MessageType.Move,
+          coords: {
+            x: enterTile.x,
+            y: enterTile.y,
+          },
+        })
+      }
+    })
   }
 
-  addPlayer(id: string, pos: position) {
-    const sprite = this.add.sprite(0, 0, "player");
+  configureWebSocket(): void {
+    this.ws = new WebsocketBuilder(
+      `${ECSB_MOVE_API_URL}/ws?gameSessionId=${sessionID}&name=${playerID}`,
+    )
+      .onOpen((i, ev) => {
+        console.log('ws opened')
+
+        sendMessage(this.ws, {
+          type: MessageType.SyncRequest,
+        })
+      })
+      .onClose((i, ev) => {
+        console.log('ws closed')
+      })
+      .onError((i, ev) => {
+        console.log('ws error')
+      })
+      .onMessage((i, ev) => {
+        const msg = parseMessage(ev.data)
+
+        if (!msg) {
+          return
+        }
+
+        switch (msg.type) {
+          case MessageType.PlayerAdded:
+            this.addPlayer(msg.id, msg.coords)
+            break
+          case MessageType.PlayerMoved:
+            this.movePlayer(msg.id, msg.coords)
+            break
+          case MessageType.PlayerRemoved:
+            this.removePlayer(msg.id)
+            break
+          case MessageType.PlayerSyncing:
+            msg.players.forEach((player) => {
+              if (player.id !== playerID) {
+                this.addPlayer(player.id, player.coords)
+              }
+            })
+            break
+        }
+      })
+      .onRetry((i, ev) => {
+        console.log('retry')
+      })
+      .build()
+  }
+
+  addPlayer(id: string, cords: Coordinates): void {
+    const sprite = this.add.sprite(0, 0, 'player')
     this.gridEngine.addCharacter({
       id: id,
       sprite: sprite,
       walkingAnimationMapping: 5,
-      startPosition: pos,
-      collides: false
-    });
+      startPosition: cords,
+      collides: false,
+    })
 
-    players[id] = pos;
+    players[id] = cords
   }
 
-  removePlayer(id: string) {
-    const sprite = this.gridEngine.getSprite(id);
+  removePlayer(id: string): void {
+    const sprite = this.gridEngine.getSprite(id)
 
-    this.gridEngine.removeCharacter(id);
-    sprite?.destroy();
-    delete players.id;
+    this.gridEngine.removeCharacter(id)
+    sprite?.destroy()
+
+    delete players.id
   }
 
-  movePlayer(id: string, pos: position) {
-    if (!players[id]) {
-      this.addPlayer(id, pos);
-      return;
-    }
+  movePlayer(id: string, cords: Coordinates): void {
+    this.gridEngine.moveTo(id, cords)
 
-    this.gridEngine.moveTo(id, pos);
-    players[id] = pos;
+    players[id] = cords
   }
 
-  update() {
-    const cursors = this.input.keyboard.createCursorKeys();
+  update(): void {
+    const cursors = this.input.keyboard.createCursorKeys()
+
     if (cursors.left.isDown) {
-      this.gridEngine.move("player", Direction.LEFT);
+      this.gridEngine.move(playerID, Direction.LEFT)
     } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", Direction.RIGHT);
+      this.gridEngine.move(playerID, Direction.RIGHT)
     } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", Direction.UP);
+      this.gridEngine.move(playerID, Direction.UP)
     } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", Direction.DOWN);
+      this.gridEngine.move(playerID, Direction.DOWN)
     }
   }
 }
