d55e84d9c5fa46cf5e75f29c828a2fe8e4f107fb
Lukasz Pawlak <lpawlak@student.agh.edu.pl>
2023-05-08 17:08:10 +0200

    [CU-862jp6k7m]
    Added player representation via stubbed constructor

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 import GridEngine from 'grid-engine'
-import { Scene } from './scenes/Scene'
-import type { GameSettings, GameStatus } from '../services/game/Types'
+import {Scene} from './scenes/Scene'
+import type {GameSettings, GameStatus} from '../services/game/Types'
 
 export const startGame = (
   gameToken: string,
@@ -10,6 +10,9 @@ export const startGame = (
 ): Phaser.Game => {
   const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
+    dom: {
+      createContainer: true
+    },
     render: {
       antialias: false,
     },
