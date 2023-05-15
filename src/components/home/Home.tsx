abecd344119146a6b8856363080745cf4d993387
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-05-15 18:30:44 +0200

    [CU-862jubuhc__Fix-joining-the-game-multiple-times]
    * Fix joining the game multiple times

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router'
 import type { NavigateFunction } from 'react-router'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import gameService from '../../services/game/GameService'
 import { decodeAuthToken, getAuthToken, getGameToken } from '../../apis/apis'
 import './Home.css'
@@ -15,51 +15,70 @@ const Home = () => {
   const { setGameData } = useGameStore()
   const [gameCode, setGameCode] = useState<string>('')
   const [error, setError] = useState<string>('')
+  const submitBtnRef = useRef<HTMLButtonElement | null>(null)
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setGameCode(event.target.value)
   }
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const enableSubmitBtn = (): void => {
+    if (submitBtnRef && submitBtnRef.current) {
+      submitBtnRef.current.disabled = false
+    }
+  }
+
+  const disableSubmitBtn = (): void => {
+    if (submitBtnRef && submitBtnRef.current) {
+      submitBtnRef.current.disabled = true
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
+    disableSubmitBtn()
+
     const authToken = getAuthToken()
-    if (!authToken) {
+    if (authToken) {
+      const decodedAuthToken = decodeAuthToken(authToken)
+      gameService
+        .getGameSession(gameCode, decodedAuthToken.name)
+        .then((res: number) => {
+          gameService
+            .getUserGameSettings()
+            .then((gameSettings: GameSettings) => {
+              gameService
+                .getUserGameStatus()
+                .then((gameStatus: GameStatus) => {
+                  const gameToken = getGameToken()
+                  if (!gameToken) {
+                    setError("Game token doesn't exist")
+                    enableSubmitBtn()
+                  } else {
+                    const gameData = startGame(gameToken, gameStatus, gameSettings)
+                    setGameData(gameData)
+                    navigate(`/game/${res}`)
+                  }
+                })
+                .catch((error: Error) => {
+                  setError(error.message)
+                  enableSubmitBtn()
+                })
+            })
+            .catch((error: Error) => {
+              setError(error.message)
+              enableSubmitBtn()
+            })
+        })
+        .catch((error: Error) => {
+          setError(error.message)
+          enableSubmitBtn()
+        })
+
       return
     }
 
-    const decodedAuthToken = decodeAuthToken(authToken)
-
-    await gameService
-      .getGameSession(gameCode, decodedAuthToken.name)
-      .then((res: number) => {
-        gameService
-          .getUserGameSettings()
-          .then((gameSettings: GameSettings) => {
-            gameService
-              .getUserGameStatus()
-              .then((gameStatus: GameStatus) => {
-                const gameToken = getGameToken()
-                if (!gameToken) {
-                  setError("Game token doesn't exist")
-                  return
-                }
-
-                const gameData = startGame(gameToken, gameStatus, gameSettings)
-                setGameData(gameData)
-                navigate(`/game/${res}`)
-              })
-              .catch((error: Error) => {
-                setError(error.message)
-              })
-          })
-          .catch((error: Error) => {
-            setError(error.message)
-          })
-      })
-      .catch((error: Error) => {
-        setError(error.message)
-      })
+    enableSubmitBtn()
   }
 
   return (
@@ -78,7 +97,11 @@ const Home = () => {
             required
           />
           {error && <span className='home-form-error'>{error}</span>}
-          <button className='home-form-btn-submit' type='submit'>
+          <button 
+            ref={submitBtnRef} 
+            className={`${submitBtnRef && submitBtnRef.current && submitBtnRef.current.disabled ? 'disabled' : ''} home-form-btn-submit`}
+            type='submit'
+          >
             JOIN THE GAME
           </button>
         </form>
