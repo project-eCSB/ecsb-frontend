fbd5ff1acd6a56d62c95881e74dd1bdb8dc804cf
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-09-27 18:56:10 +0200

    Create 'UserDataView' class that shows user's data

--- src/Game/views/EquipmentView.ts
+++ src/Game/views/EquipmentView.ts
@@ -1,84 +1,73 @@
-import { type Scene } from '../scenes/Scene'
+import { type Equipment } from "../../services/game/Types";
 
 export class EquipmentView {
-  scene: Scene
+  public static readonly equipmentBoxID = 'equipmentBox';
+  public static readonly equipmentBoxContentID = 'equipmentBoxContent';
+
   equipmentBox: HTMLDivElement
-  userName: HTMLHeadingElement
-  equipmentBoxContent: HTMLDivElement
 
-  constructor(scene: Scene) {
-    this.scene = scene
+  constructor(eq: Equipment) {
     this.equipmentBox = document.createElement('div')
-    this.equipmentBox.id = 'equipmentBox'
+    this.equipmentBox.id = EquipmentView.equipmentBoxID
 
-    this.userName = document.createElement('h3')
-    this.userName.innerText = this.scene.playerId
-
-    this.equipmentBoxContent = document.createElement('div')
-    this.equipmentBoxContent.id = 'equipmentBoxContent'
-
-    this.equipmentBox.appendChild(this.userName)
-
-    this.fillEq()
+    this.fillEq(eq)
   }
 
-  update(): void {
-    document.getElementById('equipmentBoxContent')?.remove()
-    this.equipmentBoxContent = document.createElement('div')
-    this.equipmentBoxContent.id = 'equipmentBoxContent'
-    this.fillEq()
+  public update(newEq: Equipment): void {
+    document.getElementById(EquipmentView.equipmentBoxContentID)?.remove()
+    this.fillEq(newEq)
   }
 
-  fillEq(): void {
-    if (!this.scene.equipment) return
+  private fillEq(eq: Equipment): void {
+    const equipmentBoxContent = document.createElement('div')
+    equipmentBoxContent.id = EquipmentView.equipmentBoxContentID
 
-    const iconBox = document.createElement('div')
-    iconBox.id = 'iconBox'
-
-    this.equipmentBoxContent.appendChild(iconBox)
-
-    for (const item of this.scene.equipment.resources) {
+    for (const item of eq.resources) {
       const itemBox = document.createElement('div')
-      itemBox.id = 'box'
 
-      const itemName = document.createElement('h5')
+      const itemName = document.createElement('h1')
       itemName.innerText = item.key
       const itemValue = document.createElement('p')
       itemValue.innerText = item.value.toString()
 
       itemBox.appendChild(itemName)
       itemBox.appendChild(itemValue)
-      this.equipmentBoxContent.appendChild(itemBox)
+
+      equipmentBoxContent.appendChild(itemBox)
     }
 
     const moneyBox = document.createElement('div')
-    moneyBox.id = 'box'
 
-    const moneyBoxName = document.createElement('h5')
-    moneyBoxName.innerText = 'Money'
+    const moneyBoxName = document.createElement('h1')
+    moneyBoxName.innerText = 'money'
     const moneyBoxValue = document.createElement('p')
-    moneyBoxValue.innerText = this.scene.equipment.money.toString()
+    moneyBoxValue.innerText = eq.money.toString()
 
     moneyBox.appendChild(moneyBoxName)
     moneyBox.appendChild(moneyBoxValue)
-    this.equipmentBoxContent.appendChild(moneyBox)
+    
+    equipmentBoxContent.appendChild(moneyBox)
 
     const timeBox = document.createElement('div')
-    timeBox.id = 'box'
 
-    const timeBoxName = document.createElement('h5')
-    timeBoxName.innerText = 'Time'
+    const timeBoxName = document.createElement('h1')
+    timeBoxName.innerText = 'time'
     const timeBoxValue = document.createElement('p')
-    timeBoxValue.innerText = this.scene.equipment.time.toString()
+    timeBoxValue.innerText = eq.time.toString()
 
     timeBox.appendChild(timeBoxName)
     timeBox.appendChild(timeBoxValue)
-    this.equipmentBoxContent.appendChild(timeBox)
 
-    this.equipmentBox.appendChild(this.equipmentBoxContent)
+    equipmentBoxContent.appendChild(timeBox)
+
+    this.equipmentBox.appendChild(equipmentBoxContent)
   }
 
-  show(): void {
+  public show(): void {
     window.document.body.appendChild(this.equipmentBox)
+  }
+
+  public close(): void {
+    document.getElementById(EquipmentView.equipmentBoxID)?.remove()
   }
 }
