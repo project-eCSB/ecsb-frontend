feff872e065473c90c63da3739e696c610af062a
Mateusz Skowron <mateusz.skowron@comarch.com>
2024-04-27 19:45:36 +0200

    Handle user without roles

---
+++ src/components/noauthroles/NoAuthRoles.tsx
@@ -0,0 +1,38 @@
+import { Navigate, useNavigate } from 'react-router-dom'
+import { decodeAuthToken, getAuthToken } from '../../apis/apis'
+import './NoAuthRoles.css'
+import authService from '../../services/auth/AuthService'
+
+const NoAuthRoles = () => {
+  const navigate = useNavigate()
+
+  const token = getAuthToken()
+  if (!token) {
+    return <Navigate to='/login' replace />
+  }
+
+  const decodedToken = decodeAuthToken(token)
+  if (decodedToken.roles.length !== 0) {
+    return <Navigate to='/' replace />
+  }
+
+  const handleLogOut = () => {
+    authService.logout()
+    navigate('/login')
+  }
+
+  return (
+    <div id="no-auth-roles-container">
+      <h1>Your account does not have any assigned roles.</h1>
+      <p>
+        Please contact your administrator to assign roles to your account,
+        or log out and log in with another account that has assigned roles.
+      </p>
+      <button onClick={handleLogOut}>
+        Log out
+      </button>
+    </div>
+  )
+}
+
+export default NoAuthRoles
--- src/components/protectedRoutes/NoAuthTokenProtectedRoute.tsx
+++ src/components/protectedRoutes/NoAuthTokenProtectedRoute.tsx
@@ -1,10 +1,15 @@
 import type { ReactElement } from 'react'
 import { Navigate } from 'react-router'
-import { getAuthToken } from '../../apis/apis'
+import { decodeAuthToken, getAuthToken } from '../../apis/apis'
 
 const NoAuthTokenProtectedRoute = ({ children }: { children: ReactElement }) => {
   const token = getAuthToken()
   if (token) {
+    const decodedToken = decodeAuthToken(token)
+    if (decodedToken.roles.length === 0) {
+      return <Navigate to='/no-auth-roles' replace />
+    }
+
     return <Navigate to='/' replace />
   }
 
