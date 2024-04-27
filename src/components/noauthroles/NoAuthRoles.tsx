d552b767feba3e77be3c16d449cda0737895ddeb
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-04-27 19:34:23 +0200

    Handle user without roles

--- src/components/noauthroles/NoAuthRoles.tsx
+++ src/components/noauthroles/NoAuthRoles.tsx
@@ -1,13 +1,7 @@
 import './NoAuthRoles.css'
 
 const NoAuthRoles = () => {
-  return (
-    <div className='modal-overlay'>
-      <div className='spinner-container'>
-        <div className='loading-spinner'></div>
-      </div>
-    </div>
-  )
+  return null
 }
 
 export default NoAuthRoles
